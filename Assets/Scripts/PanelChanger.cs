d19d7336e171ab80f4116bacdc938112d02e3ace
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-10-06 20:18:36 +0900

    Add OnRightArrow
    右矢印をクリックすると1つ右の画面に移動する（4面でループ）

--- Assets/Scripts/PanelChanger.cs
+++ Assets/Scripts/PanelChanger.cs
@@ -7,4 +7,29 @@ public class PanelChanger : MonoBehaviour
 {
     string currentPanelStr = "Panel0";
 
+    public void OnRightArrow()
+    {
+        //もし現在地がPanel0だったら
+        if (currentPanelStr == "Panel0")
+        {
+            //Panel1を表示する（-1000,0,0）
+            this.transform.localPosition = new Vector2(-1000, 0);
+            currentPanelStr = "Panel1";
+        }
+        else if (currentPanelStr == "Panel1")
+        {
+            this.transform.localPosition = new Vector2(-2000, 0);
+            currentPanelStr = "Panel2";
+        }
+        else if (currentPanelStr == "Panel2")
+        {
+            this.transform.localPosition = new Vector2(-3000, 0);
+            currentPanelStr = "Panel3";
+        }
+        else
+        {
+            this.transform.localPosition = new Vector2(0, 0);
+            currentPanelStr = "Panel0";
+        }
+    }
 }
