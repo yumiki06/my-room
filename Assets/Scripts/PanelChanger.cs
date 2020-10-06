fe5bd4124e949ed2ffad8c2e7687ccc1922f99e3
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-10-06 20:15:53 +0900

    Create PanelChanger class
    移動ボタンのUIを作成、表示するパネルを変数で定義

--- Assets/Scripts/PanelChanger.cs
+++ Assets/Scripts/PanelChanger.cs
@@ -1,4 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+//矢印をクリックしたら、特定のPanelを表示する
+public class PanelChanger : MonoBehaviour
+{
+    string currentPanelStr = "Panel0";
+
 }
