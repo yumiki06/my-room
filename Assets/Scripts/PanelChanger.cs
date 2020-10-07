dbdc1eeb1f5ffc3a484eea9af33283a611896e1c
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-10-07 21:42:22 +0900

    Fix OnRightArrow

--- Assets/Scripts/PanelChanger.cs
+++ Assets/Scripts/PanelChanger.cs
@@ -35,55 +35,45 @@ public class PanelChanger : MonoBehaviour
     }
     Panel currentPanel = Panel.Panel0;
 
-    public void OnRightArrow()
+    //表示するパネルを関数で定義し、引数を渡す
+    void ShowPanel(Panel nextPanel)
     {
-        //もし現在地がPanel0だったら
-        if (currentPanelStr == "Panel0")
+        currentPanel = nextPanel;
+        if (nextPanel == Panel.Panel0)
         {
-            //Panel1を表示する（-1000,0,0）
-            this.transform.localPosition = new Vector2(-1000, 0);
-            currentPanelStr = "Panel1";
+            this.transform.localPosition = new Vector2(0, 0);
         }
-        else if (currentPanelStr == "Panel1")
+        else if (nextPanel == Panel.Panel1)
+        {
+            this.transform.localPosition = new Vector2(-1000, 0);
+        }
+        else if (nextPanel == Panel.Panel2)
         {
             this.transform.localPosition = new Vector2(-2000, 0);
-            currentPanelStr = "Panel2";
-        }
-        else if (currentPanelStr == "Panel2")
-        {
-            this.transform.localPosition = new Vector2(-3000, 0);
-            currentPanelStr = "Panel3";
         }
         else
         {
-            this.transform.localPosition = new Vector2(0, 0);
-            currentPanelStr = "Panel0";
+            this.transform.localPosition = new Vector2(-3000, 0);
         }
     }
 
-    public void OnLeftArrow()
+    public void OnRightArrow()
     {
-        //もし現在地がPanel0だったら
-        if (currentPanelStr == "Panel0")
+        if (currentPanel == Panel.Panel0)
         {
-            //Panel3を表示する（-1000,0,0）
-            this.transform.localPosition = new Vector2(-3000, 0);
-            currentPanelStr = "Panel3";
+            ShowPanel(Panel.Panel1);
         }
-        else if (currentPanelStr == "Panel3")
+        else if (currentPanel == Panel.Panel1)
         {
-            this.transform.localPosition = new Vector2(-2000, 0);
-            currentPanelStr = "Panel2";
+            ShowPanel(Panel.Panel2);
         }
-        else if (currentPanelStr == "Panel2")
+        else if (currentPanel == Panel.Panel2)
         {
-            this.transform.localPosition = new Vector2(-1000, 0);
-            currentPanelStr = "Panel1";
+            ShowPanel(Panel.Panel3);
         }
         else
         {
-            this.transform.localPosition = new Vector2(0, 0);
-            currentPanelStr = "Panel0";
+            ShowPanel(Panel.Panel0);
         }
     }
 
