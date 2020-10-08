6a829fffebc55f1771149b860407c5180a2ba487
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-10-08 20:28:50 +0900

    Add enum & items
    クリックすると何らかの変化があるオブジェクト名を列挙

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -2,4 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//アイテムをクリックすると、ItemBoxに格納され、最初の場所から消える
+public class Item : MonoBehaviour
+{
+    //アイテムを列挙型で管理
+    public enum Type
+    {
+        Wallpaper, Cat, //Panel0
+        Hammer, Rubber, //Panel1
+        Vase, VaseBroken, SilverKey, GoldKey, ToolboxButton, Art, //Panel2
+        //Panel3（無し）
+
+        //Panel0（無し）
+        CatFood, Screwdriver, //Panel1
+        RightEye, //Pane2
+        LeftEye, Dirt, Match, Paper, //Panel3
+    }
 }
