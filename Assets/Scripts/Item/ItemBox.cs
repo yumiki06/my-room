9d0a0e311efdde8df737621fbd83302c333e0e2d
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-10-08 22:23:24 +0900

    Reference for studying

--- Assets/Scripts/Item/ItemBox.cs
+++ Assets/Scripts/Item/ItemBox.cs
@@ -1,4 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+        /*　配列を使用することで、アイテムの数だけ条件分岐させる必要がなくなる
+        if (type == Item.Type.Hammer)
+        {
+            box0.SetActive(true);
+            boxes[0].SetActive(true);
+        }
+        */
+        /*　配列を使用することで、アイテムの数だけ条件分岐させる必要がなくなる
+        if (type == Item.Type.Hammer)
+        {
+            return box0.activeSelf;
+        }
+        return false;
+        */
 }
