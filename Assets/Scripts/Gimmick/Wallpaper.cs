29127570ae82e7b67d6b77dddbf9f6e119e73a4e
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-10-09 20:52:40 +0900

    Create Wallpaper.cs
    クリックすると消える

---
+++ Assets/Scripts/Gimmick/Wallpaper.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wallpaper : MonoBehaviour
+{
+    public void OnThis()
+    {
+        gameObject.SetActive(false);
+    }
+}
