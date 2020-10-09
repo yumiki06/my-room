2ee92d141893719218925dc4b98e281de938e75f
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-10-09 20:53:26 +0900

    Fix enum
    取得できるアイテムのみに修正

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -8,15 +8,9 @@ public class Item : MonoBehaviour
     //アイテムを列挙型で管理
     public enum Type
     {
-        Wallpaper, Cat, //Panel0
-        Hammer, Rubber, //Panel1
-        Vase, VaseBroken, SilverKey, GoldKey, ToolboxButton, Art, //Panel2
-        //Panel3（無し）
-
-        //Panel0（無し）
-        CatFood, Screwdriver, //Panel1
-        RightEye, //Pane2
-        LeftEye, Dirt, Match, Paper, //Panel3
+        Hammer, Rubber, CatFood, Screwdriver, //Panel1
+        SilverKey, GoldKey, RightEye, //Panel2
+        LeftEye, Match, Paper, //Panel3
     }
 
     public Type type;
