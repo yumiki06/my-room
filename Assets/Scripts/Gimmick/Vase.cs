1cec6379380d1aebfa1f862333f34e3445771571
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-10-12 03:29:45 +0900

    Add UnityEvent
    アイテム使用時に音を鳴らす為

--- Assets/Scripts/Gimmick/Vase.cs
+++ Assets/Scripts/Gimmick/Vase.cs
@@ -1,23 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-//クリック時にHummerを持っていたら、Vaseが消えて、VaseBrokeが表示される
+// クリック時にHummerを持っていたら、Vaseが消えて、VaseBrokeが表示される
 public class Vase : MonoBehaviour
 {    
     public GameObject vaseBroken;
+    public UnityEvent BrokenAction;
 
     public void OnThis()
     {
-        //ItemBoxにHummerがあるか探し、持っていたら
+        // ItemBoxにHummerがあるか探し、持っていたら
         bool hasItem = ItemBox.instance.CanUseItem(Item.Type.Hammer);
         if (hasItem == true)
         {
-            //Vaseが消えて
+            // Vaseが消えて
             gameObject.SetActive(false);
-            //VaseBrokenが表示される
+            // VaseBrokenが表示される
             vaseBroken.SetActive(true);
-            //アイテムを使う
+            // 音が鳴る
+            BrokenAction.Invoke();
+
+            // アイテムを使う
             ItemBox.instance.UseItem(Item.Type.Hammer);
         }
     }
