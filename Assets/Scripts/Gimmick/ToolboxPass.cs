1c23d717eaaaf6ca4863e452b28de1130289fdb3
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-10-11 19:18:12 +0900

    Add CheckPassword
    入力されたパスワードをforeachで取り出し、正解と比較する

--- Assets/Scripts/Gimmick/ToolboxPass.cs
+++ Assets/Scripts/Gimmick/ToolboxPass.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 using UnityEngine.UI;
 
 // 数字をクリックすると1ずつ数字が増える
+// 4桁の数字が正解なら、箱が開く
 public class ToolboxPass : MonoBehaviour
 {
     public Text[] texts;
+    
     public void ChangeText(int n)
     {
-        // n番目のボタンの中のテキストが1ずつ増える
+        // n番目のボタンの数字が1ずつ増える
         int i = int.Parse(texts[n].text) +1;
         // 10以上の場合は0を代入する（9まで行ったら0に戻る）
         if(i >= 10)
@@ -17,4 +19,14 @@ public class ToolboxPass : MonoBehaviour
             i = 0;
         }
         texts[n].text = i.ToString();
+        CheckPassword();
     }
+
+    public void CheckPassword() 
+    {
+        string correctPassword = "3250";
+        string inputPassword = "";
+        foreach(Text text in texts)
+        {
+            inputPassword += text.text;
+        }
