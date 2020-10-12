f0e99ba5959b75a649708990bbfbce8d55d54d95
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-10-12 20:46:12 +0900

    Add DrawerPass
    ToolboxPassと同様に、ボタンを押すと既定のアルファベットが1つずつ動く関数を作成

---
+++ Assets/Scripts/Gimmick/DrawerPass.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class DrawerPass : MonoBehaviour
+{
+    private string chars = "ACEGHIOPTU";
+    public Text[] texts;
+    private int[] nums = {0,0,0,0,0,0,0,0,0,0};
+
+    public void ChangeText(int n)
+    {
+        // n番目のボタンのテキストが1つずつ先に進む
+        nums[n] += 1;
+        // 9番目まで行ったら0番目に戻る）
+        if(nums[n] >= chars.Length)
+        {
+            nums[n] = 0;
+        }
+        texts[n].text = chars[nums[n]].ToString();
