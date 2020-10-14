5a8f1cdc16c72affddecf44288f0cbe84dfbba01
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-10-15 02:10:51 +0900

    Add override
    共通の処理はItemから呼び出し、特殊な処理のみを記述

--- Assets/Scripts/Item/Hammer.cs
+++ Assets/Scripts/Item/Hammer.cs
@@ -1,7 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Hammer : Item
 {
+    public GameObject vaseBroken;
+    public UnityEvent usedAction;
+
+    public override void UseItem(bool active)
+    {
+        {
+            // VaseBrokenが表示される
+            vaseBroken.SetActive(true);
+            // 音が鳴る
+            usedAction.Invoke();
+            base.UseItem(active);
+        }
+    }
 }
