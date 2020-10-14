52aaa979cdea750193020dc0c82bea1168500037
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-10-15 03:13:00 +0900

    Update LeftEye.cs

--- Assets/Scripts/Item/LeftEye.cs
+++ Assets/Scripts/Item/LeftEye.cs
@@ -1,7 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class LeftEye : Item
 {
+    public GameObject leftEye;
+    public UnityEvent usedAction;
+
+    public override void UseItem(bool active)
+    {
+        {
+            // leftEyeが表示される
+            leftEye.SetActive(true);
+            // 音が鳴る
+            usedAction.Invoke();
+            base.UseItem(active);
+        }
+    }
 }
