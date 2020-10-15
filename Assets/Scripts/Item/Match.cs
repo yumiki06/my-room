a62fd7aa6d051b625962e44eb13fb636a7818e50
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-10-15 17:49:40 +0900

    Update Match.cs

--- Assets/Scripts/Item/Match.cs
+++ Assets/Scripts/Item/Match.cs
@@ -4,4 +4,14 @@ using UnityEngine;
 
 public class Match : Item
 {
+    public GameObject action;
+    public override void UseItem(bool active)
+    {
+        if(selected.activeSelf)
+        {
+            // 画像が表示される
+            action.SetActive(true);
+            base.UseItem(active);
+        }
+    }
 }
