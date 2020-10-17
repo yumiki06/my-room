3c06d0eb13b7cc4a698f2425bc6464be1604ad3f
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-10-17 19:07:20 +0900

    Update LeftEye.cs

--- Assets/Scripts/Item/LeftEye.cs
+++ Assets/Scripts/Item/LeftEye.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class LeftEye : Item
 {
     public GameObject action;
+    public GameObject appearAction;
     public override void UseItem(bool active)
     {
         if(selected.activeSelf)
         {
             // 画像が表示される
             action.SetActive(true);
+            appearAction.SetActive(true);
             base.UseItem(active);
         }
     }
