b873376f1375663e66f40d0eac35efdfafcc3675
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-10-17 19:07:10 +0900

    Update GoldKey.cs

--- Assets/Scripts/Item/GoldKey.cs
+++ Assets/Scripts/Item/GoldKey.cs
@@ -5,12 +5,12 @@ using UnityEngine.Events;
 
 public class GoldKey : Item
 {
-    public UnityEvent OpenAction;
+    public GameObject action;
     public override void UseItem(bool active)
     {
         if(selected.activeSelf)
         {
-            OpenAction.Invoke();
+            action.SetActive(true);
             base.UseItem(active);
         }
     }
