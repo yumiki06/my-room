a14fb1403cb6c20f8279577483b1f8e7392a8fdb
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-10-17 19:08:13 +0900

    Update CatFood.cs

--- Assets/Scripts/Item/CatFood.cs
+++ Assets/Scripts/Item/CatFood.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class CatFood : Item
 {
-    public GameObject action;
+    public GameObject dishAction;
+    public GameObject dissapearAction;
+    public GameObject dissapearAction1;
+    public GameObject appearAction;
     public override void UseItem(bool active)
     {
         if(selected.activeSelf)
         {
-            // 画像が表示される
-            action.SetActive(true);
+            dishAction.SetActive(true);
+            dissapearAction.SetActive(false);
+            dissapearAction1.SetActive(false);
+            appearAction.SetActive(true);
             base.UseItem(active);
         }
     }
