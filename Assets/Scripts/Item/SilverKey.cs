37f0590906e34278a783293a97f0fe750b813917
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-10-18 16:06:03 +0900

    Add dissapearAction

--- Assets/Scripts/Item/SilverKey.cs
+++ Assets/Scripts/Item/SilverKey.cs
@@ -6,12 +6,14 @@ using UnityEngine.Events;
 public class SilverKey : Item
 {
     public UnityEvent OpenAction;
+    public GameObject dissapearAction;
     public override void UseItem(bool active)
     {
         if(selected.activeSelf)
         {
             OpenAction.Invoke();
             base.UseItem(active);
+            dissapearAction.SetActive(false);
         }
     }
 }
